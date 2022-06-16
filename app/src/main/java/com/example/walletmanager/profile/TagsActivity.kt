e9cf43d940344745272ad84d29241dec77012797
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-06-15 23:19:06 -0700

    tags input taken

--- app/src/main/java/com/example/walletmanager/profile/TagsActivity.kt
+++ app/src/main/java/com/example/walletmanager/profile/TagsActivity.kt
@@ -1,11 +1,16 @@
 package com.example.walletmanager.profile
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewGroup
+import android.widget.EditText
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.walletmanager.R
 import com.example.walletmanager.dao.WalletManagerDatabase
@@ -20,6 +25,8 @@ class TagsActivity : AppCompatActivity(), TagsAdapter.OnClicked {
     private val tagsAdapter by lazy { TagsAdapter(emptyList(), this) }
 
     private val profileViewModel by viewModels<ProfileViewModel> { profileModelFactory }
+
+    private lateinit var tagInput: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,14 +49,26 @@ class TagsActivity : AppCompatActivity(), TagsAdapter.OnClicked {
     private fun setViews() {
         tags_recycler_view.layoutManager = LinearLayoutManager(this)
         tags_recycler_view.adapter = tagsAdapter
+
+        tagInput = EditText(this)
     }
 
     private fun setListeners() {
 
         add_tag_button.setOnClickListener {
-            val tag = Tag(id = System.currentTimeMillis(), tagName = "My New Tag")
-            Log.e("TAG", "tag button clicked")
-            profileViewModel.insertTag(tag)
+//            val tag = Tag(id = System.currentTimeMillis(), tagName = "My New Tag")
+            if(tagInput.parent != null) {
+                (tagInput.parent as ViewGroup).removeView(tagInput)
+            }
+            AlertDialog.Builder(this).setView(tagInput).setTitle("Enter the name of your tag")
+                .setPositiveButton("Yes") { dialogInterface, _ ->
+                    val tag = Tag(id = System.currentTimeMillis(), tagName = tagInput.text.toString())
+                    profileViewModel.insertTag(tag)
+                    dialogInterface.cancel()
+                }.setNegativeButton("No") { dialogInterface, _ ->
+                    dialogInterface.cancel()
+                }.show();
+            tagInput.setText("")
         }
 
         activity_tags_back_btn.setOnClickListener {
